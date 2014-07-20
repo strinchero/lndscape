13cc77bb393593b7ec9d2dfc5b4c35d401e99ae5
strinchero <trinchero@gmail.com>
2014-07-20 18:35:13 +0200

    t2

--- src/lndscape.js
+++ src/lndscape.js
@@ -5,42 +5,45 @@
 console.log('defining lndscape(1)');
 define("lndscape",[],function()
 {
-    this._size=size;
-    this._heightmap=null;
+    return function(size){
     
-    function smooth(fsize)
-    {
-        var count=0.0;
-        var total=0.0;
-
-        for(var x=0,xl=_heightmap.length;x<xl;x++)
+        this._size=size;
+        this._heightmap=null;
+    
+        function smooth(fsize)
         {
-            for(var y=0,yl=_heightmap[x].length;y<yl;y++)
-            {
-                count=0.0;
-                total=0.0;
+            var count=0.0;
+            var total=0.0;
 
-                for(var x0 = x - fsize; x0 < x+fsize; x0++)
+            for(var x=0,xl=_heightmap.length;x<xl;x++)
+            {
+                for(var y=0,yl=_heightmap[x].length;y<yl;y++)
                 {
-                    if(x0 < 0 || x0 > xl-1)
+                    count=0.0;
+                    total=0.0;
+
+                    for(var x0 = x - fsize; x0 < x+fsize; x0++)
+                    {
+                        if(x0 < 0 || x0 > xl-1)
+                                continue;
+
+                        for(var y0 = y - fsize; y0 < y + fsize; y0++)
+                        {
+                            if(y0 < 0 || y0 > yl-1)
                             continue;
 
-                    for(var y0 = y - fsize; y0 < y + fsize; y0++)
-                    {
-                        if(y0 < 0 || y0 > yl-1)
-                        continue;
-
-                        total += _heightmap[x0][y0];
-                        count++;
+                            total += _heightmap[x0][y0];
+                            count++;
+                        }
                     }
-                }
 
-                if(count !== 0 && total !== 0.0)
-                {
-                    _heightmap[x][y]=total/count;
+                    if(count !== 0 && total !== 0.0)
+                    {
+                        _heightmap[x][y]=total/count;
+                    }
                 }
             }
         }
-    }
+    };
     });
 })();
